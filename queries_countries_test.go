7f997af5a15f50bab74cea91c88442e5d066f796
Alexey Popov <6425762@gmail.com>
2019-01-27 00:58:30 +0300

    Golint warnings fixed

--- queries_countries_test.go
+++ queries_countries_test.go
@@ -8,7 +8,7 @@ import (
 func TestCountryCodeQuery_Get(t *testing.T) {
 	countryCode, err := New(getUserName()).CountryCode(47.03, 10.2).Get()
 	if err != nil {
-		t.Errorf("Can't recieve country code, cause %s", err.Error())
+		t.Errorf("Can't get country code, cause %s", err.Error())
 	}
 
 	if countryCode != "AT" {
@@ -21,7 +21,7 @@ func TestCountryInfoQuery_Get(t *testing.T) {
 
 	countries, err := New(getUserName()).CountryInfo(countryCodes...).Get()
 	if err != nil {
-		t.Errorf("Can't recieve country info (for %s), cause %s", strings.Join(countryCodes, ","), err.Error())
+		t.Errorf("Can't get country info (for %s), cause %s", strings.Join(countryCodes, ","), err.Error())
 	}
 
 	mustLen := len(countryCodes)
