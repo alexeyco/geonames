b152420f3d042852e7fae07f0a83fd7a33d3a51c
Alexey Popov <6425762@gmail.com>
2019-01-26 03:09:25 +0300

    Examples

--- postal_code_query.go
+++ postal_code_query.go
@@ -1,6 +1,9 @@
 package geonames
 
-import "net/url"
+import (
+	"errors"
+	"net/url"
+)
 
 // See http://www.geonames.org/export/web-services.html#postalCodeSearch
 
@@ -25,7 +28,7 @@ func (q PostalCodeQuery) Countries(countries ...string) PostalCodeQuery {
 }
 
 // Get executes query
-func (q PostalCodeQuery) Get() ([]Place, error) {
+func (q PostalCodeQuery) Get() ([]PostalCode, error) {
 	v := url.Values{}
 	v.Add("postalcode", q.postalCode)
 
@@ -39,5 +42,61 @@ func (q PostalCodeQuery) Get() ([]Place, error) {
 		}
 	}
 
-	return q.query.execute("postalCodeSearchJSON", v)
+	res := response{}
+	if err := q.query.execute("postalCodeSearchJSON", v, &res); err != nil {
+		return []PostalCode{}, err
+	}
+
+	if res.Error != nil {
+		return []PostalCode{}, errors.New(res.Error.Message)
+	}
+
+	return res.Places, nil
+}
+
+// PostalCodeByPlaceNameQuery list of postal codes and places for the place name
+type PostalCodeByPlaceNameQuery struct {
+	query                Query
+	placeName            string
+	postalCodeStartsWith string
+	countries            []string
+}
+
+// PostalCodeStartsWith sets the first characters of a postal code
+func (q PostalCodeByPlaceNameQuery) PostalCodeStartsWith(startsWith string) PostalCodeByPlaceNameQuery {
+	q.postalCodeStartsWith = startsWith
+	return q
+}
+
+// Countries restricts searching by specified countries
+func (q PostalCodeByPlaceNameQuery) Countries(countries ...string) PostalCodeByPlaceNameQuery {
+	q.countries = countries
+	return q
+}
+
+// Get executes query
+func (q PostalCodeByPlaceNameQuery) Get() ([]PostalCode, error) {
+	v := url.Values{}
+	v.Add("placename", q.placeName)
+
+	if q.postalCodeStartsWith != "" {
+		v.Add("postalcode_startsWith", q.postalCodeStartsWith)
+	}
+
+	if len(q.countries) > 0 {
+		for _, c := range q.countries {
+			v.Add("country", c)
+		}
+	}
+
+	res := response{}
+	if err := q.query.execute("postalCodeSearchJSON", v, &res); err != nil {
+		return []PostalCode{}, err
+	}
+
+	if res.Error != nil {
+		return []PostalCode{}, errors.New(res.Error.Message)
+	}
+
+	return res.Places, nil
 }
--- query.go
+++ query.go
@@ -2,7 +2,6 @@ package geonames
 
 import (
 	"encoding/json"
-	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -13,8 +12,8 @@ type Query struct {
 	userName string
 }
 
-// FindByPostalCode find place by postal code
-func (q Query) FindByPostalCode(postalCode string) PostalCodeQuery {
+// FindPostalCode find postal code
+func (q Query) FindPostalCode(postalCode string) PostalCodeQuery {
 	return PostalCodeQuery{
 		query:      q,
 		postalCode: postalCode,
@@ -22,43 +21,38 @@ func (q Query) FindByPostalCode(postalCode string) PostalCodeQuery {
 	}
 }
 
-// FindByPlaceName find place by name
-func (q Query) FindByPlaceName(placeName string) PlaceNameQuery {
-	return PlaceNameQuery{
+// FindPostalCodeByPlaceName find postal code by place name
+func (q Query) FindPostalCodeByPlaceName(placeName string) PostalCodeByPlaceNameQuery {
+	return PostalCodeByPlaceNameQuery{
 		query:     q,
 		placeName: placeName,
 		countries: []string{},
 	}
 }
 
-func (q Query) execute(e string, v url.Values) ([]Place, error) {
+func (q Query) execute(e string, v url.Values, result interface{}) error {
 	req, err := q.request(e, v)
 	if err != nil {
-		return []Place{}, err
+		return err
 	}
 
 	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		return []Place{}, err
+		return err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return []Place{}, err
+		return err
 	}
 
-	result := response{}
 	if err := json.Unmarshal(body, &result); err != nil {
-		return []Place{}, err
+		return err
 	}
 
-	if result.Error != nil {
-		return []Place{}, errors.New(result.Error.Message)
-	}
-
-	return result.Places, nil
+	return nil
 }
 
 func (q Query) request(e string, v url.Values) (*http.Request, error) {
